0f64de5dba3a410696c268c563b07d05edebdd12
Tim Martin <tim@asymptotic.co.uk>
2015-10-15 20:50:25 +0100

    Wrote a setup.py for distribution

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+NAME = "TrivialUI"
+PACKAGES = find_packages(exclude=["tests*"])
+CLASSIFIERS = [
+    "Development Status :: 1 - Planning",
+    "Intended Audience :: Developers",
+    "Natural Language :: English",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Hy",
+    "Operating System :: OS Independent",
+    "Topic :: Software Development :: User Interfaces"
+]
+INSTALL_REQUIRES = []
+
+if __name__ == '__main__':
+    setup(name=NAME,
+          version="0.0.1",
+          classifiers=CLASSIFIERS,
+          install_requires=INSTALL_REQUIRES,
+          packages=PACKAGES)
